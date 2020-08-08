5d187b4591997b029565c3cd6fea6eed967a4c13
unknown <stefiniv4j@gmail.com>
2020-08-08 17:35:40 +0300

    JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/helloworld/HelloWorld.java
+++ chapter_001/src/main/java/ru/job4j/helloworld/HelloWorld.java
@@ -1,6 +1,18 @@
 package ru.job4j.helloworld;
 
+/**
+ * Class HelloWorld решения задачи части 001 урок1.
+ *
+ * @author Pavlo Stefiniv
+ * @version $Id$
+ * @since 08.08.2020
+ */
 public class HelloWorld {
+    /**
+     * Метод для вывода Hello World в консоль.
+     *
+     * @param args - args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
